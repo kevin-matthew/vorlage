cbaecf8ac4772b5ee0386b311e66266ed25d5ddc
kevin marschke <kevin-fake-email@oidag.com>
2020-02-16 23:42:44 -0600

    added caching interface

--- gosrc/doccomp/caching.go
+++ gosrc/doccomp/caching.go
@@ -1,10 +1,22 @@
 package doccomp
 
+type Cache interface {
 
-func GetCached(request Request) *Document {
-	return nil
-}
+	/*
+	 * this is asked every request. If true is returned, a call to AddToCache
+	 * will follow. If false is returned, a call to GetFromCache will follow.
+	 * On error, neither is called.
+	 */
+	ShouldCache(path string) (bool, error)
 
-func addToCache(Document) {
+	/*
+	 * add a document to the cache. it should be able to be indexed by using
+	 * it's path from d.GetFilePath
+	 */
+	AddToCache(d Document) error
 
+	/*
+	 * Load the document from the cache by using its path.
+	 */
+	GetFromCache(path string) (*Document, error)
 }
