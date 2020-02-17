379b771bd4894f18d4a234bd6b937ce99b0f76ed
kevin marschke <kevin-fake-email@oidag.com>
2020-02-17 00:36:33 -0600

    fixed debug logs

--- gosrc/doccomp/debuging.go
+++ gosrc/doccomp/debuging.go
@@ -4,7 +4,8 @@ import "fmt"
 
 func Debugf(format string, args ...interface{}) {
 	message := fmt.Sprintf(format, args...)
-	_, _ = fmt.Printf(format, "[ %s%.6s \033[0m] %s\n", "\033[1;36m",
+	_, _ = fmt.Printf("[ %s%.6s \033[0m] %s\n",
+		"\033[1;36m",
 		"DEBUG",
 		message)
 }
