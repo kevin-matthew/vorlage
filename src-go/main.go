372b90396eb8c100b7a2aeec28fc5bb99804670c
kevin marschke <kevin-fake-email@oidag.com>
2020-03-12 16:16:03 -0500

    testing a scanning/drawing method

--- src-go/main.go
+++ src-go/main.go
@@ -2,9 +2,25 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 	"strconv"
 	"strings"
 )
+
+const MacroArgument = ' ' //todo: just rename this to 'macrospace'
+const MacroPrefix = "#"
+const DefineStr = "#define"
+const PrependStr = "#prepend"
+const AppendStr = "#append"
+const EndOfLine = "\n"
+const VariablePrefix = "$("
+const VariableSuffix = ")"
+const VariableProcessorSeporator = "."
+const VariableRegexp = `^(?:[a-z0-9]+\.)?[a-zA-Z0-9]+$`
+const MacroMaxLength = 1024
+const MaxVariableLength = 32
+
+var variableRegexpProc = regexp.MustCompile(VariableRegexp)
 
 type Error struct {
 	ErrStr  string
@@ -27,6 +43,8 @@ func (e Error) Error() string {
 	return ret
 }
 
+// only set subject to a variable that was passed through the arguments of
+//the scope.
 func (e *Error) SetSubject(subject string) {
 	e.Subject = subject
 }
@@ -61,83 +79,211 @@ func (e Error) ErrorHighlight() string {
 var errNotImplemented = &Error{ErrStr: "not implemented"}
 
 const (
-	errFailedToReadBytes = "failed to read bytes from stream"
-	errFailedToSeek      = "failed to seek through file"
-	errNotDefined        = "a variable was left undefined"
+	errFailedToReadBytes            = "failed to read bytes from stream"
+	errFailedToReadDocument         = "failed to read from document"
+	errFailedToReadPrependDocument  = "failed to read from prepended document"
+	errFailedToReadAppendedDocument = "failed to read from appended document"
+	errFailedToReadVariable         = "failed to read variable"
+	errFailedToSeek                 = "failed to seek through file"
+	errConvert                      = "could not convert file"
+	errNotDefined                   = "a variable was left undefined"
+	errResetVariable                = "failed to reset variable"
+	errAlreadyDefined               = "variable has already been defined"
+	errVariableTooLong              = "variable too long"
+	errVariableMissingSuffix        = "'$(' detected but no ')'"
+	errVariableMissingPrefix        = "'$(' not detected"
+	errBufferTooShort               = "the buffer is too short"
+	errVariableDraw                 = "cannot draw variable"
+	errVariableName                 = "variable has an invalid name"
 )
 
-type macoPos struct {
-	args  []string
-	charPos uint64
-	length  uint
-	linenum uint
-}
+// returns nil,nil if that means you should keep scanning
+// returns pos,nil if a variable was found
+// returns nil,err if an error happened while parsing
+func drawParseVar(dest []byte, src []byte,
+	charsource int64,
+	linenum uint,
+	colnum uint) (pos *variablePos, oerr *Error) {
 
-//const EndOfLine   = "\n#"
-const MacroArgument = ' '  //todo: just rename this to 'macrospace'
-const MacroPrefix = "#"
-const DefineStr  = "define"
-const PrependStr = "prepend"
-const AppendStr  = "append"
-const EndOfLine = "\n"
-const VariablePrefix = "$"
-const MacroMaxLength = 1024
-const MaxVariableLength = 32
+	// we retain i for 2 reasons: 1) we can check of a loop completed and 2)
+	// so if we have just scanned in the start of a new variable from src to
+	// dest, we can start the normal scanning proccess from where we left off
+	// when we discovered the start of the variable.
+	var i = 0
 
-func bytesAreString(buff []byte, str string, offset int) bool {
-	return offset+len(str) <= len(buff) &&
-		string(buff[offset:offset+len(str)]) == str
-}
+	var j = 0
 
-// helper-function for detectMacrosPositions
-// simply looks at the buffer and scans a macro out of it. It returns the
-// length of the line, and any possible error. If 0 length is returned,
-// no more macros are left to scan.
-func scanMaco(buffer []byte, charsource int64,
-	linenum uint) (pos macoPos, oerr *Error) {
-
-	// first off, do we even have a valid macro?
-	if !bytesAreString(buffer, MacroPrefix, 0) {
-		// no this isn't a macro... so we're done looking for macros.
-		return pos,nil
+	// if the dest starts with null (0), then that means we haven't started
+	// drawing a variable yet. So look at src to see if (and where) we should
+	// start.
+	if dest[0] != VariablePrefix[0] {
+		for ; i < len(src) && src[i] != VariablePrefix[0]; i++ {
+		}
+		if i == len(src) {
+			// we're not recording a variable, nor did we find the start of one
+			// in src.
+			return nil, nil
+		}
 	}
 
+	// at this point we've just found, or have previously found at least
+	// the start of a variable that is currently loaded in dest.
 
+	// so lets find where we left off with dest (when dest[j] == 0 that means
+	// we havent written to that part of it yet)
+	for ; j < len(dest) && dest[j] != 0; j++ {
+	}
 
-	// get length
-	pos.linenum = linenum
-	pos.charPos = uint64(charsource)
-	pos.length = uint(len(MacroPrefix)) // we skip scanning the macro prefix
-	for ; pos.length < uint(len(buffer)); pos.length++ {
-		// grab the end of the line
+	// now appended src to dest
+	for j < len(dest) && i < len(src) {
+		dest[j] = src[i]
+		j++
+		i++
+	}
 
-		// first see if we can get to '\n'...
-		if bytesAreString(buffer, EndOfLine, int(pos.length)) {
-			// cut out the end of the line
-			pos.length += uint(len(EndOfLine))
+	// if the scanned in bytes is shorter than the prefix, then
+	// we need to wait another scan.
+	if j < len(VariablePrefix) {
+		return nil, nil
+	}
+
+	scannedPos, serr := scanVariable(dest, charsource, linenum, colnum)
+	if serr != nil {
+		switch serr.ErrStr {
+		case errVariableMissingSuffix:
+			// so we didn't scan in a full variable into dest...
+			// now we ask: are we out of room in dest?
+			if j == len(dest) {
+				// if we are, then the caller can't draw anymore. so send em the
+				// error
+				for j = 0; j < len(dest); j++ {
+					dest[j] = 0
+				}
+				return nil, serr
+			}
+			// if we're not at the end of dest then the caller can call this
+			// function more times until we indeed fill it.
+			return nil, nil
+		case errVariableMissingPrefix:
+			// theres no prefix. which means the buffer is crap if it doesn't
+			// even start right. So throw the whole thing away.
+			for j = 0; j < len(dest); j++ {
+				dest[j] = 0
+			}
+			return nil, nil
+
+		}
+		return nil, serr
+	}
+	for j = 0; j < len(dest); j++ {
+		dest[j] = 0
+	}
+	return &scannedPos, nil
+}
+
+// helper-function for detectVariables
+// looks at the buffer and tries to parse a variable out of it.
+// The itself variable must start at the very beginning of the buffer.
+func scanVariable(buffer []byte, charsource int64,
+	linenum uint, colnum uint) (pos variablePos, oerr *Error) {
+
+	if len(buffer) < len(VariablePrefix)+len(VariableSuffix) {
+		// this buffer isn't big enough to even consider the possibility
+		// of having a variable.
+		return pos, NewError(errBufferTooShort)
+	}
+
+	var length, j, dotIndex int
+	for length = 0; length < len(VariablePrefix); length++ {
+		if buffer[length] != VariablePrefix[length] {
+			// no valid prefix, no variable to be found here!
+			return pos, NewError(errVariableMissingPrefix)
+		}
+	}
+
+	for ; length < len(buffer); length++ {
+		// keep scanning through until we find the VariableSuffix
+		if length+len(VariableSuffix) >= len(buffer) {
+			// The VariableSuffix was not found in this buffer.
+			oerr = NewError(errVariableMissingSuffix)
+			return pos, oerr
+		}
+
+		for j = 0; j < len(VariableSuffix); j++ {
+			if buffer[length+j] != VariableSuffix[j] {
+				break
+			}
+		}
+		if j == len(VariableSuffix) {
+			length = length + j
 			break
 		}
 	}
-	if pos.length <= uint(len(MacroPrefix)) {
-		oerr = &Error{}
-		oerr.ErrStr = "macro prefix detected but nothing defined"
-		oerr.SetSubjectf("line %d", pos.linenum)
+
+	varName := buffer[len(VariablePrefix) : length-len(VariableSuffix)]
+
+	if !variableRegexpProc.Match(varName) {
+		oerr = NewError(errVariableName)
+		oerr.SetSubjectf("'%s' at line %d", string(varName), linenum)
 		return pos, oerr
 	}
 
-	// todo: what if macro is to long
-	//append(pos.args, )
-	tmp := strings.Split(string(buffer[:pos.length]), string(MacroArgument))
-	pos.args = []string{}
-	for _,t := range tmp {
-		if t != "" {
-			pos.args = append(pos.args, t)
-		}
+	dotIndex = strings.Index(string(buffer[:length]),
+		VariableProcessorSeporator)
+	if dotIndex == -1 {
+		dotIndex = 0
 	}
-	return pos,nil
+
+	pos = variablePos{
+		fullName:     string(buffer[:length]),
+		variableName: string(buffer[len(VariablePrefix) : length-len(VariableSuffix)]),
+		processorName: string(buffer[len(VariablePrefix) : len(
+			VariablePrefix)+dotIndex]),
+		charPos: charsource,
+		length:  uint(length),
+		linenum: linenum,
+		colnum:  colnum,
+	}
+	return pos, nil
+}
+
+type variablePos struct {
+	fullName     string
+	variableName string // this will be the Processor-Variable Name if
+	// processorName is not ""
+	processorName string // if "" then it is not a processed variable
+	charPos       int64
+	length        uint
+	linenum       uint // used for debugging
+	colnum        uint // used for debugging
+}
+
+func (v variablePos) ToString() string {
+	return fmt.Sprintf("'%s', line %d, col %d", v.fullName, v.linenum, v.colnum)
 }
 
 func main() {
-	p,e := scanMaco([]byte("#a"), 0,0)
-	fmt.Printf("%s/%v",strings.Join(p.args, "-"),e)
+	dest := make([]byte, 28)
+	var bufsize = 3
+	src := make([]byte, bufsize)
+	str := "Hello, this is my $(var) and I like to fuck $(jesus." +
+		"GodDamn) and now I present to you $(bullshit"
+	for i := 0; i < len(str); i += len(src) {
+		for j := 0; j < len(src); j++ {
+			src[j] = str[i+j]
+		}
+		pos, err := drawParseVar(dest, src, 0, 0, 0)
+		if err != nil {
+			fmt.Printf("'%s' - \"%s\" - (err=%s)\n", string(src), string(dest),
+				err.Error())
+		} else if pos != nil {
+			fmt.Printf("'%s' - \"%s\" - (pos=%s)\n", string(src), string(dest),
+				pos.fullName)
+		} else {
+			fmt.Printf("'%s' - \"%s\" - (pos=nil,err=nil)\n", string(src),
+				string(dest))
+		}
+
+	}
+
 }
