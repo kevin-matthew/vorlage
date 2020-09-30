0f584c06a85c8f9485e390c90c60d0d8f10bfbaa
kevin marschke <kevin-fake-email@oidag.com>
2020-09-30 15:55:03 -0500

    trying to fix the bug where variables that were too close to gether wouldn't all get read.
    I know the issue lied somewhere in the tmpBuff logic being read and then being used as a source buffer. So I agregated tmpBuff reads with the source file reads. So that way bytes from either one go through the same logic that's used to parse and define variables
    
    I also cleaned up the code a bit... this move I'm sure will cause more bugs (hopefully not)

--- reader.go
+++ reader.go
@@ -2,11 +2,9 @@ package doccomp
 
 import "io"
 
-func (c *nonConvertedFile) Read(dest []byte) (n int, err error) {
-	if c.hasEOFd {
-		return 0, io.EOF
-	}
-
+// helper function for nonConvertedFile.Read
+// returns io.EOF if the definition has been completely read from.
+func (c *nonConvertedFile) readDefinition(dest []byte) (n int, err error) {
 	// are we currently exposing a defnition?
 	if c.currentlyReadingDef != nil {
 		// we are... so lets read it.
@@ -17,100 +15,146 @@ func (c *nonConvertedFile) Read(dest []byte) (n int, err error) {
 			}
 			// we're done reading the current definition.
 			c.currentlyReadingDef = nil
-
-			dest = dest[n:]
-
-			//return n, nil
+			return n, io.EOF
 		} else {
 			// we're not done reading the definition yet.
 			return n, nil
 		}
 	}
+	return 0, io.EOF
+}
 
-	// before we do another read from the file, we need to make sure that
-	// tmpBuff has been emptied.
+// reads from the source file, or, reads from tmpBuff if that is not empty.
+func (c *nonConvertedFile) readSource(dest []byte) (n int, err error) {
+	// tmpBuff has priority over the file because tmpBuff is filled with
+	// bytes that we previously read from the source file that we cannot read
+	// again.
 	if len(c.tmpBuff) != 0 {
-		bytesCopied := copy(dest, c.tmpBuff)
-		c.tmpBuff = c.tmpBuff[bytesCopied:]
-		dest = dest[bytesCopied:]
-		n += bytesCopied
-		if len(c.tmpBuff) != 0 {
-			// if we still have stuff in tmp buffer, we need another read.
-			return n, nil
-		}
+		// tmpBuff was not empty. we must read from it.
+		n = copy(dest, c.tmpBuff)
+		c.tmpBuff = c.tmpBuff[n:]
+		return n, nil
 	}
 
-	// so if we're done reading the definition, move along with another read
-	// from the file
+	// tmpBuff is empty, read from the source file
+	n, err = c.sourceFile.Read(dest)
+	// increment the total amount of bytes read from the source file
+	c.bytesRead += int64(n)
+	return n, err
+}
 
-	var sourceBytesRead int
-	sourceBytesRead, err = c.sourceFile.Read(dest)
+func (c *nonConvertedFile) Read(dest []byte) (totalBytes int, err error) {
+	if c.hasEOFd {
+		return 0, io.EOF
+	}
+	var n int
 
-	// we WILL NOT append this bytes to n. Because if we do there's a chance
+	// first, read any definition that we may be in currently.
+	n, err = c.readDefinition(dest)
+	totalBytes += n
+	if err != io.EOF {
+		// err may be nil here, which means that the definition is not
+		// done reading.
+		// likewise it may be non-nil with an actual error. in which case
+		// we need to return it anyways.
+		return totalBytes, err
+	}
+	// truncate the part of the buffer to which we've already written too...
+	// you'll see this a lot in this funciton.
+	dest = dest[n:]
+
+	// sense we're not reading from a definition, we can read from the source
+	// file (or the tmpBuff if available.)
+	n, err = c.readSource(dest)
+	// we WILL NOT append this bytes to totalBytes. Because if we do there's a chance
 	// we've read in a variable. We don't want that to show up for the caller.
-	//n+=sourceBytesRead
+	// totalBytes += n (see above why this was commented out)
 	if err != nil && err != io.EOF {
-		return n, err
+		return totalBytes, err
 	}
 	// set hasEOFd so future calls will return EOF.
 	c.hasEOFd = err == io.EOF
-	c.bytesRead += int64(sourceBytesRead)
 
-	// do the draw parse into variable buffer from what we've read from the
-	// source stream.
-	nonVarByteCount, pos, cerr := drawParseVar(c.variableReadBuffer, dest[:sourceBytesRead], c.bytesRead)
+	// now lets check to see if the source file gave us any variables to chew
+	// on. Also, You will not understand the following code until you understand
+	// drawParseVar.
+	nonVarByteCount, pos, cerr := drawParseVar(c.variableReadBuffer,
+		dest[:n],
+		c.bytesRead)
+	// now here we can acutally add these bytes to what has been read.
+	totalBytes += nonVarByteCount
 	if cerr != nil {
-		return n + nonVarByteCount, *cerr
+		// some error happened that made parsing impossible, such as a missing
+		// suffix.
+		return totalBytes, *cerr
 	}
-	if nonVarByteCount == sourceBytesRead {
-		return n + nonVarByteCount, err
+	if nonVarByteCount == n {
+		// the entire buffer was found to have no variables in it.
+		// (err is returned because it may be an io.EOF from previous calls)
+		return totalBytes, err
 	}
 	if pos != nil {
-		// we have stumbled apon a variable.
-		def, derr := c.sourceDocument.define(*pos)
-		if derr != nil {
-			return n + nonVarByteCount, derr
-		}
+		// if we're in here... we have scanned in a full variable that follows
+		// all the syntax rules.
 
-		// lets start reading it on the next read.
-		c.currentlyReadingDef = def
-
-		// but first we ask:
-		// did the buffer (dest) pick up anything after the variable?
+		// Before we begin reading the variable's definition, we must ask:
+		// did the buffer (dest) pick up anything AFTER the variable?
+		//
 		// (ie dest[:n] = "123$(varible)abc")
-		//                    ^         ^  ^
-		//                   (a)       (b)(c)
+		//                    ^        ^  ^
+		//                   (a)      (b)(c)
 		//
 		//  (a) = position of nonVarByteCount
 		//  (b) = position of nonVarByteCount + len(pos.fullName)
-		//  (c) = position of sourceBytesRead (length of string)
-		// if so, we need to save the extra (ie "abc") to tmpBuff because
+		//  (c) = position of n (length of string)
+		//
+		// we need to save the extra (ie "abc") to tmpBuff because
 		// dest will be used to read-in the variable and will in turn all
 		// content that was read after the variable from the file.
-		if sourceBytesRead > nonVarByteCount+len(pos.fullName) {
+		if n > nonVarByteCount+len(pos.fullName) {
 
-			// calculate the remaining buffer length
-			remainingBuffLen := sourceBytesRead - (nonVarByteCount + len(pos.fullName))
+			// so in here we now that n (c) is bigger than (b). Which means
+			// there's something after the variable that we've scanned in.
+			// lets move it all into tmpBuff.
+
+			// calculate the remaining buffer length (c-b)
+			remainingBuffLen := n - (nonVarByteCount + len(pos.fullName))
 			// make the tmp buff the size of everything after the variable.
 			c.tmpBuff = make([]byte, remainingBuffLen)
 			// copy everything after that variable into that buffer.
-			copy(c.tmpBuff, dest[nonVarByteCount+len(pos.fullName):sourceBytesRead])
-
-			// lets use up the rest of the buffer we were given in this call
-			// to try to fill the next one. You could comment these three
-			// lines out and the only thing it would really affect is the
-			// fact that the caller needs to call Read a few more times.
-			//lmlog.AlertF("%s", string(dest[nonVarByteCount:]))
-			bytesOfDefinition, err := c.Read(dest[nonVarByteCount:])
-			n += bytesOfDefinition + nonVarByteCount
-			return n, err
+			copy(c.tmpBuff, dest[nonVarByteCount+len(pos.fullName):n])
 		}
-		return n + nonVarByteCount, err
+
+		// first go back to the Document and find this variable's definition
+		def, derr := c.sourceDocument.define(*pos)
+		if derr != nil {
+			// many errors can occour here... for intance, the variable
+			// does not exist, the processor doesn't exist, invalid input, ect.
+			return totalBytes, derr
+		}
+		// lets start reading it on the next read by setting c.currentlyReadingDef
+		// to a non-nil value (see readDefinition)
+		c.currentlyReadingDef = def
+
+		// This next if statment is completely optional.
+		// all this does is ask if there's any more space in dest we haven't
+		// used. Without this if statment, the caller would just have to call
+		// Read more often... so this ooptimizaiton is just for the caller's
+		// convenience
+		if len(dest) > nonVarByteCount {
+			// lets use up the rest of the buffer we were given in /this/ call
+			// to try to fill the /next/ one.
+			secondCallBytesRead, err := c.Read(dest[nonVarByteCount:])
+			totalBytes += secondCallBytesRead
+			return totalBytes, err
+		}
+		return totalBytes, err
 	}
 
 	// at this point we know that a variable was not found, but not all bytes were
-	// ignored.
-	return n + nonVarByteCount, err
+	// ignored. Which means we STARTED to scan a variable but we need to call
+	// another read to grab the rest of it.
+	return totalBytes, err
 }
 
 func (c *nonConvertedFile) Rewind() error {
