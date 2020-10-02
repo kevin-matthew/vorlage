35ef8428e5e8eaf82fe82edd2e6785732f32c358
kevin marschke <kevin-fake-email@oidag.com>
2020-10-01 19:11:16 -0500

    okay. I figured out how we're going do be doing sessions in doccomp.

--- http/conf.go
+++ http/conf.go
@@ -30,4 +30,16 @@ var TryFiles []string = []string{"index.html", "index.proc.html"}
  * Proc indicator. The file extension to look for that will activate
  * the processing. Otherwise a normal file request will take place.
  */
-const FileExt = ".proc.html"
+var FileExt = ".proc.html"
+
+/*
+ * if SessionCookie is enabled, a HttpOnly cookie will be created with the value
+ * of 32 random bytes (in the form of 64 random hex values) and will retain on
+ * the user's browser indefinitely. It's value can be accessed via the input
+ * of __SESSIONCOOKIE.
+ * Session is the same thing except the cookie will have the "Session" flag and
+ * can be accessed by __SESSION.
+ * Having both enabled will result in 2 cookies.
+ */
+var SessionCookie = true
+var Session = true
