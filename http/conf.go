6cd80c8a725ca7fa213684dd67aafe183b7a56e8
kevin marschke <kevin-fake-email@oidag.com>
2020-10-29 16:38:20 -0500

    authentication defintiion

--- http/conf.go
+++ http/conf.go
@@ -33,6 +33,18 @@ var TryFiles []string = []string{"index.html", "index.proc.html"}
 var FileExt []string = []string{".proc.html", ".proc.json"}
 
 /*
+ * If a requested file (regardless of its existance) is located as a decendent
+ * of any directory name found in AuthDirectories, http's basic auth header
+ * will be used. When this happens, the ValidAuth callback will be used. If
+ * ValidAuth returns false, a 403 will be returned to the request.
+ * If ValidAuth is null, 403 will always be returned.
+ * The relm of the basic authentication will be the same as the directory
+ * name to which had invoked the auth request.
+ */
+var AuthDirectories []string = []string{}
+var ValidAuth func(username string, password string) bool = nil
+
+/*
  * if SessionCookie is enabled, a HttpOnly cookie will be created with the value
  * of 32 random bytes (in the form of 64 random hex values) and will retain on
  * the user's browser indefinitely. It's value can be accessed via the input
