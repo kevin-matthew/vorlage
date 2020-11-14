a97dcdb49c6892a643163a64b09b7a7ef1407893
kevin marschke <kevin-fake-email@oidag.com>
2020-11-14 16:51:51 -0600

    started drafting the true processor interface

---
+++ c.src/processors.h
@@ -0,0 +1,30 @@
+#ifndef VORLAGE_PROCESSORS_H_
+#define VORLAGE_PROCESSORS_H_ 1
+#include "vorlage.h"
+
+
+// below are structs that must be returned by processors during the vorlage
+// request cycle.
+typedef struct {
+
+} vorlage_proc_info;
+typedef struct {
+
+} vorlage_proc_actions;
+typedef struct {
+
+} vorlage_proc_definer;
+typedef struct {
+
+} vorlage_proc_exitinfo;
+
+// if you're making a processor, you must define these:
+const vorlage_proc_info     vorlage_startup  (const vorlage_execinfo    *sinfo);
+const vorlage_proc_actions  vorlage_onrequest(const vorlage_requestinfo *rinfo);
+const vorlage_proc_definer  vorlage_define   (const vorlage_defineinfo  *dinfo);
+const vorlage_proc_exitinfo vorlage_shutdown (const vorlage_execinfo    *sinfo);
+
+
+
+
+#endif /* VORLAGE_PROCESSORS_H_ */
