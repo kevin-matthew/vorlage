f78ff185b295e91355eef017502a7dcd523a3ae2
kevin marschke <kevin-fake-email@oidag.com>
2020-11-14 19:08:30 -0600

    I am on a ROLL with finishing this project.

--- c.src/processors.h
+++ c.src/processors.h
@@ -2,15 +2,86 @@
 #define VORLAGE_PROCESSORS_H_ 1
 #include "vorlage.h"
 
+const int vorlage_proc_interfaceversion = 0x0
 
-// below are structs that must be returned by processors during the vorlage
-// request cycle.
+/*
+ * vorlage_proc_info is given to vorlage the instant a processor is loaded.
+ */
 typedef struct {
+	// Describe what this processor does and why someone would need
+	// it.
+	const char *description;
 
+	// Specify what input field names this processor needs during the
+	// request phase (can be nil)
+	vorlage_inputproto       *inputproto;
+	vorlage_streaminputproto *streaminputproto;
+
+
+	// an array of variables that this processor provides to
+	// documents.
+	vorlage_variable *variablesv
+	int               variablesc
 } vorlage_proc_info;
-typedef struct {
 
+/*
+ * vorlage_proc_action (and its array wrapper vorlage_proc_actions)
+ * is a struct to which can specify actions to perform before the
+ * request is even executed.
+ * The list of actions can be found in vorlage_proc_actionenum along
+ * with the each action's documentation.
+ */
+enum vorlage_proc_actionenum {
+
+	// The processor has hit a critical error that is it's own fault.
+	// This action will stop the request. vorlage_proc_action.data can
+	// be set to a null-terminated string that will be shown to the
+	// user.
+	VORLAGE_PROC_ACTION_CRITICAL,
+	
+	// The processor recongizes that the request is a violation of the
+	// access granted to the user. vorlage_proc_action.data can be
+	// set to a null-term string that will be shown to the user.
+	// tip: use this in conjunction with VORLAGE_PROC_ACTION_SEE to
+	//      invoke a redirect to a longin page.
+	VORLAGE_PROC_ACTION_ACCESSFAIL,
+
+	// The processor request that the user see another
+	// file. vorlage_proc_action.data must be set to a file path to
+	// which the user will be directed to.
+	VORLAGE_PROC_ACTION_SEE,
+
+	
+	/**** HTTP only ****/
+	
+	// The processor will set a cookie to the user's
+	// browser. vorlage_proc_action.data must be a null-term string
+	// that is a valid cookie syntax defined in rfc6265 section 4.1.1.
+	// vorlage_proc_action.data must NOT include header name. (don't
+	// dictate the "Set-Cookie:" part but dictate everything after
+	// that)
+	VORLAGE_PROC_ACTION_HTTPCOOKIE,
+};
+// action struct (see above enum)
+typedef struct {
+	// action is an int/enum that is equal to any item found in the
+	// aciton list (see VORLAGE_PROC_
+	enum vorlage_proc_actionenum action;
+
+	// data is arbitrary data that is context-specific to whatever
+	// action was set. So see the aformentioned action list.
+	void *data;
+} vorlage_proc_action;
+// action array struct (see above struct)
+typedef struct {
+	vorlage_proc_action *actionv;
+	int                  actionc;
 } vorlage_proc_actions;
+
+
+
+
+
 typedef struct {
 
 } vorlage_proc_definer;
---
+++ c.src/vorlage.h
@@ -0,0 +1,19 @@
+#ifndef VORLAGE_H_
+#define VORLAGE_H_ 1
+
+
+
+/*
+ * vorlage_requestinfo is provided to processors
+ */
+typedef struct {
+
+} vorlage_requestinfo;
+
+
+typedef struct {
+
+} vorlage_defineinfo;
+
+
+#endif /* VORLAGE_H_ */
