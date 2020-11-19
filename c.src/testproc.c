80b8034b18dc765cb9919f0973b7bd1b713995c1
kevin marschke <kevin-fake-email@oidag.com>
2020-11-18 18:32:28 -0600

    started to implement proc_define

--- c.src/testproc.c
+++ c.src/testproc.c
@@ -51,10 +51,42 @@ const vorlage_proc_actions  vorlage_proc_onrequest(const vorlage_proc_requestinf
 	return v;
 };
 
+typedef struct {
+	char *buffer;
+	int pos;
+} customstream;
+static ssize_t cust_read(void *cookie, char *buf, size_t size) {
+	// todo!!!! see man(3) fopencookie	
+}
+static int seek(void *cookie, off64_t *offset, int whence) {
+	// todo!!!! see man(3) fopencookie
+}
+static int cust_close(void *cookie) {
+	c = (customstream *)cookie;
+	free(c->buffer);
+	return 0;
+}
 
-const vorlage_proc_definer  vorlage_proc_define(const vorlage_proc_defineinfo  dinfo){
-vorlage_proc_definer v = {0};
-return v;
+const int  vorlage_proc_define(const vorlage_proc_defineinfo  dinfo){
+	const char *whattoecho = dinfo.input[0];
+	const char *prefix     = "this is what you're echoing:";
+
+	// dealloc'd in cust_close (called in voralge)
+	char *newstr = malloc(strlen(whattoecho) + strlen(prefix) + 1);
+	strcat(newstr, prefix);
+	strcat(&(newstr[strlen(whattoecho)]), prefix);
+	customstream c = {
+		.buffer = newstr,
+		.pos = 0,
+	};
+	cookie_io_functions_t funcs = {
+		.read = cust_read,
+		.write = 0,
+		.seek = cust_seek,
+		.close = cust_close,
+	};
+	FILE *f = fopencookie(&c, 'r', funcs);
+	return fileno(f);
 };
 
 
