41476b352c6b9018bd0b2d9e539f78d5f8413754
kevin marschke <kevin-fake-email@oidag.com>
2020-12-22 19:07:11 -0600

    removed vorlage-buffer option

--- http/http.go
+++ http/http.go
@@ -8,108 +8,106 @@ import (
 	"os"
 )
 
-
-
 var DocumentRoot string = "."
-var BindAddress string  = "localhost:80"
-var UseFcgi bool        = false
+var BindAddress string = "localhost:80"
+var UseFcgi bool = false
 var ConfigFile = "/etc/vorlage/http.conf"
 
 var config = []conf.ConfigBinding{
 	{
-		Name: "http-documentroot",
+		Name:        "http-documentroot",
 		Description: "the document root where the server will run at",
-		VarAddress: &DocumentRoot,
+		VarAddress:  &DocumentRoot,
 	},
 	{
-		Name: "http-bindaddress",
+		Name:        "http-bindaddress",
 		Description: "the address that vorlage will bind onto",
-		VarAddress: &BindAddress,
+		VarAddress:  &BindAddress,
 	},
 	{
-		Name: "http-usefcgi",
+		Name:        "http-usefcgi",
 		Description: "if true, vorlage will bind to http-bindaddress as an fcgi application. this is a long description because fuck you and fuck me we're all going to die and forgotten in 200 years.",
-		VarAddress: &UseFcgi,
+		VarAddress:  &UseFcgi,
 	},
 	{
-		Name: "http-buffer-multipart",
+		Name:        "http-buffer-multipart",
 		Description: "The maximum memory allocated during multipart requests.",
-		VarAddress: &MultipartMaxMemory,
+		VarAddress:  &MultipartMaxMemory,
 	},
+	//{
+	//	Name: "vorlage-buffer",
+	//	Description: "The size of the buffer that is streamed from the disk through vorlage per request.",
+	//	VarAddress: &ProcessingBufferSize,
+	//},
 	{
-		Name: "vorlage-buffer",
-		Description: "The size of the buffer that is streamed from the disk through vorlage per request.",
-		VarAddress: &ProcessingBufferSize,
-	},
-	{
-		Name: "vorlage-ldpath",
+		Name:        "vorlage-ldpath",
 		Description: "A path to a directory to which vorlage will search for available vorlageprocs.",
-		VarAddress: &vorlage.CLoadPath,
+		VarAddress:  &vorlage.CLoadPath,
 	},
 	{
-		Name: "vorlage-goldpath",
+		Name:        "vorlage-goldpath",
 		Description: "A path to a directory to which vorlage will search for available go vorlageprocs.",
-		VarAddress: &vorlage.GoPluginLoadPath,
+		VarAddress:  &vorlage.GoPluginLoadPath,
 	},
 	{
-		Name: "log-debug",
+		Name:        "log-debug",
 		Description: "If set, will output debug information to the file. Note that outputting debug information must only be done when, well, debugging. Enabling debugging may cause dramatic slow downs.",
-		VarAddress: &logs.Debug,
+		VarAddress:  &logs.Debug,
 	},
 	{
-		Name: "log-verbose",
+		Name:        "log-verbose",
 		Description: "If set, will output verbose information to the selected file.",
-		VarAddress: &logs.Verbose,
+		VarAddress:  &logs.Verbose,
 	},
 	{
-		Name: "log-warnings",
+		Name:        "log-warnings",
 		Description: "If set, will output warnings to the selected file. An warnings constitutes any behaviour that can lead to errors and/or failures.",
-		VarAddress: &logs.Warnings,
+		VarAddress:  &logs.Warnings,
 	},
 	{
-		Name: "log-errors",
+		Name:        "log-errors",
 		Description: "If set, will output errors to the selected file. An error constitutes any unintended behaviour that was caused by user input.",
-		VarAddress: &logs.Errors,
+		VarAddress:  &logs.Errors,
 	},
 	{
-		Name: "log-failures",
+		Name:        "log-failures",
 		Description: "If set, will output failures to the selected file. A failure constitutes any unintended behaviour that wasn't caused by the user input. A failure can also be referred to as a bug.",
-		VarAddress: &logs.Failures,
+		VarAddress:  &logs.Failures,
 	},
 	{
-		Name: "log-timestamps",
+		Name:        "log-timestamps",
 		Description: "If true, log files will be given timestamps on each entry. Useless when debugging, really useful when going live.",
-		VarAddress: &logs.Timestamps,
+		VarAddress:  &logs.Timestamps,
 	},
 }
 
-
 var mainlogContext logcontext
 var httplogContext logcontext
+
 func main() {
 	// configure
 	if err := conf.BindAll(config); err != nil {
-		_,_ = fmt.Fprintf(os.Stderr, "failed to start configuring: " + err.Error())
+		_, _ = fmt.Fprintf(os.Stderr, "failed to start configuring: "+err.Error())
 		os.Exit(1)
 	}
 	if len(os.Args) == 2 && os.Args[1] == "--help" {
 
-		_,_  = fmt.Printf("usage: %s [--ARGUMENT=VALUE]... [CONFIG_FILE]\n", os.Args[0])
+		_, _ = fmt.Printf("usage: %s [--ARGUMENT=VALUE]... [CONFIG_FILE]\n", os.Args[0])
 		fmt.Printf("Valid --ARGUMENT=VALUE pairs:\n")
 		// load the config file so that help menu shows the default values after
 		// the configure file has been loaded.
 		_ = conf.LoadConfFile(ConfigFile)
-		_,_ = fmt.Fprintf(os.Stdout, conf.HelpArgs())
+		_, _ = fmt.Fprintf(os.Stdout, conf.HelpArgs())
 
-		_,_ = fmt.Fprintf(os.Stderr, "Note: The above arguments can be pre-set in the CONFIG_FILE\n")
-		_,_ = fmt.Fprintf(os.Stderr, "      as ARGUMENT=VALUE pairs.\n")
-		_,_ = fmt.Fprintf(os.Stderr, "Note: The default CONFIG_FILE location is %s\n", ConfigFile)
+		_, _ = fmt.Fprintf(os.Stderr, "Note: The above arguments can be pre-set in the CONFIG_FILE\n")
+		_, _ = fmt.Fprintf(os.Stderr, "      as ARGUMENT=VALUE pairs.\n")
+		_, _ = fmt.Fprintf(os.Stderr, "Note: The default CONFIG_FILE location is %s\n", ConfigFile)
 		os.Exit(0)
 	}
 
 	if len(os.Args) == 2 && os.Args[1] == "--defaults" {
 
-		_,_  = fmt.Printf("%s", conf.HelpFile())
+		_, _ = fmt.Printf("%s", conf.HelpFile())
 		os.Exit(0)
 	}
 
@@ -119,20 +117,20 @@ func main() {
 	}
 
 	if err := conf.LoadConfFile(ConfigFile); err != nil {
-		_,_ = fmt.Fprintf(os.Stderr, err.Error() + "\n")
-		_,_ = fmt.Fprintf(os.Stderr, "See " + os.Args[0] + " --help\n")
+		_, _ = fmt.Fprintf(os.Stderr, err.Error()+"\n")
+		_, _ = fmt.Fprintf(os.Stderr, "See "+os.Args[0]+" --help\n")
 		os.Exit(1)
 	}
 
 	if err := conf.LoadConfArgs(os.Args[1:]); err != nil {
-		_,_ = fmt.Fprintf(os.Stderr, err.Error() + "\n")
-		_,_ = fmt.Fprintf(os.Stderr, "See " + os.Args[0] + " --help\n")
+		_, _ = fmt.Fprintf(os.Stderr, err.Error()+"\n")
+		_, _ = fmt.Fprintf(os.Stderr, "See "+os.Args[0]+" --help\n")
 		os.Exit(1)
 	}
 	// configuration complete.
 	// now lets set up our logging
 	if err := logs.LoadChannels(); err != nil {
-		_,_ = fmt.Fprintf(os.Stderr, "failed to open log file: " + err.Error() + "\n")
+		_, _ = fmt.Fprintf(os.Stderr, "failed to open log file: "+err.Error()+"\n")
 		os.Exit(1)
 	}
 	mainlogContext = logcontext{
@@ -141,10 +139,9 @@ func main() {
 	}
 	httplogContext = logcontext{
 		context: "http",
-		c: &logs,
+		c:       &logs,
 	}
 	mainlogContext.Infof("logs configured")
-
 
 	// bind to the address we'll be using for http request
 	mainlogContext.Infof("binding to address %s...", BindAddress)
@@ -186,9 +183,9 @@ func main() {
 	mainlogContext.Infof("starting server for document root \"%s\"...", DocumentRoot)
 	err = Serve(l, procs, UseFcgi, DocumentRoot)
 	if err != nil {
-		mainlogContext.Infof( "http server exited with error: %s", err.Error())
+		mainlogContext.Infof("http server exited with error: %s", err.Error())
 		os.Exit(1)
 		return
 	}
-	mainlogContext.Infof( "vorlage http server closed without error")
+	mainlogContext.Infof("vorlage http server closed without error")
 }
