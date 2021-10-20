255076842425b5b25db4c588fe42619505611c65
kevin marschke <kevin-fake-email@oidag.com>
2021-10-20 16:45:15 -0500

    changed the specification to say that the processor is indeed allowed to name itself.

--- vorlage-interface/golang/processor-interface2.go
+++ vorlage-interface/golang/processor-interface2.go
@@ -1,9 +1,4 @@
 package golang
 
-// Your build target must have one of these defined to be picked up by vorlage.
-// If both are defined, VorlageGo will be used.
-//
-// If either of these are declared, then the old processor intface is ignored.
-//
-// func VorlageGo() vorlageproc.VorlageGo { }
-// func VorlageGov() []vorlageproc.VorlageGo { }
+// Your build target must have this defined to be picked up by vorlage:
+//   func VorlageGoV() []vorlageproc.VorlageGo { }
